391fba4013c6bd9fd673d469d3c3694157917a6d
Fosink <1473582326@qq.com>
2023-03-15 14:56:51 +0800

    Question

---
+++ src/main/java/com/rjsj/pethospital/service/QuestionService.java
@@ -0,0 +1,20 @@
+package com.rjsj.pethospital.service;
+
+import com.rjsj.pethospital.entity.Case;
+import com.rjsj.pethospital.entity.Question;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface QuestionService {
+
+    List<Question> findAll();
+    List<Question> findAllByType(String type);
+    Optional<Question> findById(Long id);
+
+    Question findByContent(String name);
+
+    Question save(Question question);
+
+    void deleteById(Long id);
+}
