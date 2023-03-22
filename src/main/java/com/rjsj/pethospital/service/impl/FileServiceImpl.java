fceb82eaebe44a92cf95e7bfc5e8745b1b58e1ca
LongNameLikeMe <2694681433@qq.com>
2023-03-22 15:33:53 +0800

    case management

--- src/main/java/com/rjsj/pethospital/service/impl/FileServiceImpl.java
+++ src/main/java/com/rjsj/pethospital/service/impl/FileServiceImpl.java
@@ -6,13 +6,14 @@ import org.springframework.web.multipart.MultipartFile;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.Objects;
 
 @Service
 public class FileServiceImpl implements FileService {
 
     @Override
     public String saveFile(String fileName, MultipartFile file) {
-        if (file == null)
+        if (file == null || Objects.equals(file.getOriginalFilename(), ""))
             return null;
         String suffix = file.getOriginalFilename().substring(file.getOriginalFilename().lastIndexOf("."));
         File newFile = new File("E:/res", fileName + suffix);
