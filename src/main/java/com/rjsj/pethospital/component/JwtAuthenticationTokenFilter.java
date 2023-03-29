485a4a52ebf0b254cfb5d83dd474127a10f1e11b
LongNameLikeMe <2694681433@qq.com>
2023-03-29 13:34:54 +0800

    fix options

--- src/main/java/com/rjsj/pethospital/component/JwtAuthenticationTokenFilter.java
+++ src/main/java/com/rjsj/pethospital/component/JwtAuthenticationTokenFilter.java
@@ -4,10 +4,12 @@ import com.rjsj.pethospital.entity.LoginUser;
 import com.rjsj.pethospital.util.JwtUtil;
 import io.jsonwebtoken.Claims;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
 import org.springframework.util.StringUtils;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import javax.servlet.FilterChain;
@@ -25,10 +27,16 @@ public class JwtAuthenticationTokenFilter extends OncePerRequestFilter {
 
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+
+        if (request.getMethod().equals(RequestMethod.OPTIONS.name())) {
+            response.setStatus(HttpStatus.OK.value());
+            return;
+        }
+
         //获取token
         String token = request.getHeader("token");
+
         if (!StringUtils.hasText(token)) {
-            //放行
             filterChain.doFilter(request, response);
             return;
         }
@@ -44,13 +52,13 @@ public class JwtAuthenticationTokenFilter extends OncePerRequestFilter {
         //从redis中获取用户信息
         String redisKey = "login:" + userid;
         LoginUser loginUser = redisCache.getCacheObject(redisKey);
-        if(Objects.isNull(loginUser)){
+        if (Objects.isNull(loginUser)) {
             throw new RuntimeException("用户未登录");
         }
         //存入SecurityContextHolder
         //TODO 获取权限信息封装到Authentication中
         UsernamePasswordAuthenticationToken authenticationToken =
-                new UsernamePasswordAuthenticationToken(loginUser,null,null);
+                new UsernamePasswordAuthenticationToken(loginUser, null, null);
         SecurityContextHolder.getContext().setAuthentication(authenticationToken);
         //放行
         filterChain.doFilter(request, response);
--- src/main/java/com/rjsj/pethospital/service/impl/LoginServiceImpl.java
+++ src/main/java/com/rjsj/pethospital/service/impl/LoginServiceImpl.java
@@ -14,7 +14,6 @@ import org.springframework.stereotype.Service;
 
 import java.util.HashMap;
 import java.util.Map;
-import java.util.Objects;
 
 @Service
 public class LoginServiceImpl implements LoginService {
